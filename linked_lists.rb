995356564b1d78a550366dab38d6a6531d3b97d0
mthsgrc <matheus.dev.garcia@gmail.com>
2021-01-12 18:56:51 -0300

    Count size list

--- linked_lists.rb
+++ linked_lists.rb
@@ -1,20 +1,16 @@
 require "pry"
-# require "enumerator"
-
 
 class LinkedList
   include Enumerable
 
-  attr_accessor :name
-
-  # @@full_list = []
+  attr_accessor :head, :tail
 
   def initialize
     @head = nil
     @tail = nil
   end
 
-  def append(data)
+  def append(data) #add to list end
     if @head.nil?
       @head = Node.new(data)
       @tail = @head
@@ -24,86 +20,80 @@ class LinkedList
     end
   end
 
-
-  def prepend(data)
+  def prepend(data) #add to list start
+    # binding.pry
     if @head.nil?
       @head = Node.new(data)
       @tail = @head
     else
+      tmp = @head
       @head = Node.new(data)
-      @head.next_node = @head
+      @head.next_node = tmp
     end
   end
 
-  def pop
-  	
+  def size
+    return "No Nodes in list" if @head.nil?
+    node = @head
+    count = 1
+    until node.next_node.nil?
+      node = node.next_node
+      count += 1
+    end
+    "List has size #{count}"
   end
 
   def print_head
-  	return "No Nodes in List" if @head.nil?
+    return "No Nodes in List" if @head.nil?
     @head.value
   end
 
   def print_tail
-  	return "No Nodes in List" if @head.nil?
+    return "No Nodes in List" if @head.nil?
     @tail.value
   end
 
-  def size
-  	until @tail.next_node == nil
- 	  		  		
-  	end
+  def value_at(index)
+    
+
+    
   end
+
+  def pop
+    binding.pry
+
+
+  end
+
+
 end
-
-
 
 class Node
   attr_accessor :value, :next_node
-  def initialize(value)
+
+  def initialize(value, next_node = nil)
     @value = value
-    @next_node = nil
+    @next_node = next_node
   end
 end
 
+
+
 list = LinkedList.new
-list.append("name")
-list.append("name2")
-list.append("name3")
-list.prepend("other")
-list.prepend("initial")
-
-# puts 
+# binding.pry
+# puts list.size
 
 
+puts list.size
+list.append(2)
+list.prepend(1)
 
-list.pop
-puts list.print_tail
-puts list.print_head
+list.append(3)
 
+list.prepend(0)
 
+list.append(4)
 
+puts list.values_at(2)
 
-# def put_top_queue(value)
-#   if @head.nil?
-#     @head = value
-#     @tail = value
-#   else
-#     value.next = @head
-#     @head = value
-#   end
-# end
-
-# def put_bottom_queue(value)
-#   if @head.nil?
-#     @head = value
-#     @tail = value
-#   else
-#     @tail.next = value
-#     @tail = value
-#   end
-# end
-
-# def remove_bottom
-
-# end
+puts list.size
