75490061a86cb22b3ee61580b286b5ad2407c395
mthsgrc <matheus.dev.garcia@gmail.com>
2021-01-14 14:17:34 -0300

    Methods pop and contains?

--- linked_lists.rb
+++ linked_lists.rb
@@ -1,7 +1,6 @@
 require "pry"
 
 class LinkedList
-  include Enumerable
 
   attr_accessor :head, :tail
 
@@ -55,24 +54,40 @@ class LinkedList
 
   def value_at(index)
     return "No Nodes in List" if @head.nil?
-
     node = @head
     count = 0
-
     until count == index
       node = node.next_node
       count += 1
     end
-
     "Value at index #{index} is #{node.value}"
   end
 
   def pop
-    binding.pry
+    return "No Nodes in List" if @head.nil?
+    @head = nil if !@head.next_node
 
-
+    prev = @head
+    node = @head.next_node
+    while node.next_node
+      node = node.next_node
+      prev = prev.next_node
+    end
+    @tail = prev
+    prev.next_node = nil
   end
 
+  def contains?(data)
+    node = @head
+    while node 
+      # binding.pry
+      if node.value == data
+        return true
+      end
+      node = node.next_node
+    end
+    false
+  end
 
 end
 
@@ -91,23 +106,39 @@ list = LinkedList.new
 
 
 
-puts list.size
 list.append(2)
+puts list.print_tail
+
 list.prepend(1)
+puts list.print_tail
 
 list.append(3)
+puts list.print_tail
 
 list.prepend(0)
+puts list.print_tail
 
 list.append(4)
 
+
 list.append(55)
+puts list.print_tail
+# puts list.size
 
-puts list.value_at(0)
-puts list.value_at(1)
-puts list.value_at(2)
-puts list.value_at(3)
+puts list.contains?(0)
+puts list.contains?(-1)
+puts list.contains?(2)
+puts list.contains?(3)
+puts list.contains?(4)
+puts list.contains?(55)
 
-puts list.value_at(5)
+
+
+# puts list.value_at(0)
+# puts list.value_at(1)
+# puts list.value_at(2)
+# puts list.value_at(3)
+
+# puts list.value_at(5)
 
 puts list.size
